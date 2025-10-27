ad1f65e864551b20954dcb81927e5553b9117fc3
robin-UI <robinbiju1901@gmail.com>
2025-10-27 20:32:33 +0530

    Script form is working

--- src/components/layout/HomeSection/ContactSection.tsx
+++ src/components/layout/HomeSection/ContactSection.tsx
@@ -18,6 +18,8 @@ const ContactSection = () => {
 
     const form = e.currentTarget;
     const formData = new FormData(form);
+    const scriptURL = "https://script.google.com/macros/s/AKfycbydA740ttqxCgeFRUunzKOYlC4IJToqYBGvzqaZXQfYWMUyFqDyvWNtIN5AWcs6lx5V/exec"
+    const zohoURL = "https://forms.zohopublic.in/adminviri1/form/PreviktaWebsite/formperma/QV6vR7uRlqBG75EVBmNvekv1nbBfI129nwqpqWlP_aE/htmlRecords/submit"
 
     // Create a hidden iframe for form submission
     const iframe = document.createElement('iframe');
@@ -27,7 +29,7 @@ const ContactSection = () => {
 
     // Create a temporary form for submission
     const tempForm = document.createElement('form');
-    tempForm.action = "https://forms.zohopublic.in/adminviri1/form/PreviktaWebsite/formperma/QV6vR7uRlqBG75EVBmNvekv1nbBfI129nwqpqWlP_aE/htmlRecords/submit";
+    tempForm.action = scriptURL;
     tempForm.method = "POST";
     tempForm.target = 'hidden_iframe';
     tempForm.encType = "multipart/form-data";
@@ -107,7 +109,8 @@ const ContactSection = () => {
         <div className="flex flex-col md:flex-row gap-4 w-full">
           <div className="w-full md:w-1/2 border-b border-[rgba(19,45,71,0.2)] pb-2">
             <input
-              name="Name_First"
+              // name="Name_First"
+              name="first_name"
               placeholder="First Name"
               required
               className="w-full text-base md:text-[18px] leading-[23px] font-[Duplet] font-semibold bg-transparent outline-none"
@@ -115,7 +118,8 @@ const ContactSection = () => {
           </div>
           <div className="w-full md:w-1/2 border-b border-[rgba(19,45,71,0.2)] pb-2">
             <input
-              name="Name_Last"
+              // name="Name_Last"
+              name="last_name"
               placeholder="Last Name"
               required
               className="w-full text-base md:text-[18px] leading-[23px] font-[Duplet] font-semibold bg-transparent outline-none"
@@ -127,7 +131,8 @@ const ContactSection = () => {
         <div className="flex flex-col md:flex-row gap-4 w-full">
           <div className="w-full md:w-1/2 border-b border-[rgba(19,45,71,0.2)] pb-2">
             <input
-              name="Email"
+              name="email"
+              // name="Email"
               required
               placeholder="Email"
               className="w-full text-base md:text-[18px] leading-[23px] font-[Duplet] font-semibold bg-transparent outline-none"
@@ -135,7 +140,8 @@ const ContactSection = () => {
           </div>
           <div className="w-full md:w-1/2 border-b border-[rgba(19,45,71,0.2)] pb-2">
             <input
-              name="PhoneNumber_countrycode"
+              // name="PhoneNumber_countrycode"
+              name="phone_number"
               required
               placeholder="Phone Number"
               className="w-full text-base md:text-[18px] leading-[23px] font-[Duplet] font-semibold bg-transparent outline-none"
@@ -146,7 +152,8 @@ const ContactSection = () => {
         {/* Message Field */}
         <div className="w-full border-b border-[rgba(19,45,71,0.2)] pb-2">
           <textarea
-            name="SingleLine"
+            // name="SingleLine"
+            name="message"
             placeholder="Message"
             className="w-full h-24 md:h-32 text-base md:text-[18px] leading-[23px] font-[Duplet] font-semibold bg-transparent outline-none resize-none"
           />
